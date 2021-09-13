cd130210eb464bb4fcc7427a9580ef8e8aa5042d
saurabhthakare9 <90328247+saurabhthakare9@users.noreply.github.com>
2021-09-14 01:01:31 +0530

    initial commit

---
+++ Primitive_Datatypes.java
@@ -0,0 +1,25 @@
+package datatypes;
+
+public class Primitive_Datatypes {
+public static void main(String[] args) {
+	
+	boolean b=false;
+	byte a= 105;
+	short c= 11555;
+	int y=1555555556;
+	long u= 55555559999999l;
+	float f=963.556f;
+	double d= 225566668.559d;
+	char i='v';
+	
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(a);
+	System.out.println(y);
+	System.out.println(u);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(i);
+	
+}
+}
